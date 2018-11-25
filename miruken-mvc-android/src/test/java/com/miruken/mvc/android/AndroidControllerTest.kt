f7ad66e04c93af01a5eace42a8c662eeb7e1d60b
Craig Neuwirt <cneuwirt@gmail.com>
2018-11-25 13:56:08 -0600

    WIP

--- miruken-mvc-android/src/test/java/com/miruken/mvc/android/AndroidControllerTest.kt
+++ miruken-mvc-android/src/test/java/com/miruken/mvc/android/AndroidControllerTest.kt
@@ -1,6 +1,8 @@
 package com.miruken.mvc.android
 
 import androidx.databinding.Bindable
+import com.miruken.callback.Provides
+import com.miruken.context.Scoped
 import com.miruken.mvc.android.databinding.bindable
 import org.junit.Test
 
@@ -8,13 +10,20 @@ class AndroidControllerTest {
     @Test fun `Creates Controller ViewModel`() {
     }
 
-    class MyController : AndroidController() {
-        inner class MyViewModel : ViewModel() {
+    class MyController
+        @Provides @Scoped
+        constructor(): AndroidController() {
+
+        inner class MyViewModel : AndroidController.ViewModel() {
             @get:Bindable
             val title by bindable("Hello", 1)
 
             fun fetchData() {
             }
+        }
+
+        fun hello() {
+            next<MyController> { hello() }
         }
     }
 }
